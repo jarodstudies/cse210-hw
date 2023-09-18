cc432ff047b318e8c4cb2de33fe72defb49b6139
jarodstudies <jarodosorioramos@gmail.com>
2023-09-18 21:29:56 +0200

    Week 2 Prep Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "doc-IT";
+        job1._jobTitle = "Support Technician";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Purple Turtle";
+        job2._jobTitle = "Designer";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+
+
+        //job1.Display();
+        //job2.Display();
+
+        Resume myresume = new Resume();
+        myresume._name = "Jarod Ramos";
+        myresume._jobs = new List<Job>();
+
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
+
+        //string myResume = resume._jobs[0]._company;
+        //Console.WriteLine(myResume);
+
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Resume
+{
+
+    public string _name;
+    public List<Job> _jobs;
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+
+
+}
